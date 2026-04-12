f6c37c6a898d10d89d667fa1a4d93dc71e1435d8
HenriqueFazendeiro <henriquemhvfazendeiro@gmail.com>
2026-04-12 18:33:54 +0100

    Adjust mobile header background

--- components/navigation/site-header.js
+++ components/navigation/site-header.js
@@ -1,7 +1,7 @@
 export function SiteHeader() {
   return (
     <header className="fixed top-0 left-0 z-50 h-20 w-full px-5 md:px-8">
-      <div className="header-blur-layer pointer-events-none fixed inset-0 -z-10 h-[4.75rem] w-full backdrop-blur-lg md:h-[11.75rem]" />
+      <div className="header-blur-layer pointer-events-none fixed inset-0 -z-10 h-[4.75rem] w-full md:h-[11.75rem] md:backdrop-blur-lg" />
 
       <div className="flex h-full items-center justify-between" />
     </header>
